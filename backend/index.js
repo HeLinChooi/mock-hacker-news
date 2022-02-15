e6fbe7455e2d9fc76d9b3a8a2ff0df5fb6b10a77
He Lin <helinchooi@gmail.com>
2022-02-15 09:27:11 +0800

    Able to run without ssl cert & fix version number

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const getPreviewData = async (url) => {
 
   const linkPreviewGenerator = require("link-preview-generator");
