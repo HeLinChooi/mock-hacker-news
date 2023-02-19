83e6e9d7c3a3667a3231e82b0f3b4e0e68800df9
He Lin <helinchooi@gmail.com>
2023-02-19 15:49:58 +0800

    Try to deploy express on vercel

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   next();
 });
 
@@ -48,3 +49,5 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+module.exports = app;
